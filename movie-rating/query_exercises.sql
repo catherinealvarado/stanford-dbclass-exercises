e42f02747eccb3f2b7ae9839f00ce29899bc48d8
calvarado <calvarado@wesleyan.edu>
2017-05-30 13:25:57 -0700

    First commit

---
+++ movie-rating/query_exercises.sql
@@ -0,0 +1,23 @@
+/* These exercises are from the Stanford SQL course.
+
+You've started a new movie-rating website, and you've been collecting data
+on reviewers' ratings of various movies. There's not much data yet, but you
+can still try out some interesting queries. Here's the schema: 
+
+Movie ( mID,  (movie with ID)
+	    title,   (movie title)
+	    year,   (release year)
+	   director (a director)
+	   )
+
+Reviewer ( rID,  (reviewer with ID number)
+		  name (reviewer name)
+	        )
+
+Rating ( rID,  (reviewer rID)
+             mID, (movie mID )
+	     stars, (number of stars rating (1-5) )
+	     ratingDate (ratingDate)
+	   )
+
+*/
