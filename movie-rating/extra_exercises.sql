ba083d6f4299f4560bbbdcaa7571eba9ed7012cf
calvarado <calvarado@wesleyan.edu>
2017-05-30 17:13:32 -0700

    Created a new file with extra exercises from the SQL course.

---
+++ movie-rating/extra_exercises.sql
@@ -0,0 +1,21 @@
+/* These exercises are from the Stanford SQL course.
+The same movie rating tables are being used, but the queries
+are extra SQL queries from the course.
+
+You've started a new movie-rating website, and you've been collecting data
+on reviewers' ratings of various movies.
+Here's the schema for each table:
+
+Movie (mID,     (Movie's unique ID)
+	     title,   (Title of the movie)
+	     year,    (Movie release year)
+	     director (Movie director))
+
+Reviewer (rID,  (Movie reviewer ID)
+		      name  (Reviewer name))
+
+Rating (rID,    (Reviewer rID)
+        mID,    (Movie's unique ID)
+	      stars,  (Number of stars rating (1-5) for movie)
+	      ratingDate (Date movie was rated))
+*/
