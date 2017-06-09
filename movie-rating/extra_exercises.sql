003e685849fafed46c5c2f57089bb8fff321ce22
calvarado <calvarado@wesleyan.edu>
2017-06-08 22:40:32 -0700

    Changed the formating of the descriptions for the SQL table.

--- movie-rating/extra_exercises.sql
+++ movie-rating/extra_exercises.sql
@@ -6,18 +6,21 @@ You've started a new movie-rating website, and you've been collecting data
 on reviewers' ratings of various movies.
 Here's the schema for each table:
 
-Movie (mID,     (Movie's unique ID)
-	     title,   (Title of the movie)
-	     year,    (Movie release year)
-	     director (Movie director))
+Movie:
+(mID,     (Movie's unique ID)
+ title,   (Title of the movie)
+ year,    (Movie release year)
+ director (Movie director))
 
-Reviewer (rID,  (Movie reviewer ID)
-		      name  (Reviewer name))
+Reviewer:
+ (rID,  (Movie reviewer ID)
+	name  (Reviewer name))
 
-Rating (rID,    (Reviewer rID)
-        mID,    (Movie's unique ID)
-	      stars,  (Number of stars rating (1-5) for movie)
-	      ratingDate (Date movie was rated))
+Rating:
+(rID,       (Reviewer rID)
+ mID,       (Movie's unique ID)
+ stars,     (Number of stars rating (1-5) for movie)
+ ratingDate (Date movie was rated))
 */
 
 --Q1: Find the names of all reviewers who rated Gone with the Wind.
